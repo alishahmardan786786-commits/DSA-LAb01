d26c4fd5f1ba1395c5d38af15528fe0c86f10318
alishahmardan786786-commits <alishahmardan786786@gmail.com>
2026-09-16 11:19:53 +0500

    DSA-LAB-02

--- task2.cpp
+++ task2.cpp
@@ -1,17 +1,50 @@
 #include <iostream>
 using namespace std;
+int totalMarks(int marks[],int n){
+    int total;
 
-int main() {
-    int arr[5];
-    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {   
+        total += *(marks + i);
+    }
+    return total;
+}
+int main()
+{   int n; 
+    int total = 0;
+    int count = 0;
+    double average;
 
-    for (int i = 0; i < 5; i++) {
-        cout << "Enter " << i << " element" << endl;
-        cin >> arr[i];
-        sum += arr[i];
+    cout << "Enter number of students : "  ;
+    cin >> n;
+
+    int* marks = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {   cout << "\nEnter marks of students " << i+1 << " : ";
+        cin >> *(marks + i);
     }
 
-    cout << "Sum of the array's elements : " << sum << endl;
+    for (int i = 0; i < n; i++)
+    {   
+        cout << "\nMarks of student " << i+1 << " is : " << *(marks + i);
+    }
+    for (int i = 0; i < n; i++)
+    { 
+        if(*(marks + i) >= 50){
+             count++;
+        }
+    }
+    
+    total = totalMarks(marks,n);
+    cout << "\nTotal marks of all students is : " << total << endl;
+   
+    average = total /(double)n;
+    cout << "Average of the students marks is : " << average  << endl;
+    
+    cout << "pass count : " << count  << endl;
+    delete[] marks; 
+    marks = nullptr;
 
-    return 0;
+    
 }
